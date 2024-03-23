1e9553502cb2fe5f41187caf7f7bcc3dad76e8cf
devika kumari <devika21@navgurukul.org>
2024-03-23 22:41:40 +0530

    update code

--- script.js
+++ script.js
@@ -1,42 +1,40 @@
 
 
+
 const search_input = document.getElementById("search-input");
 const search_btn = document.getElementById("search-btn");
 const result = document.getElementById("result");
 
 search_btn.addEventListener("click", () => {
     let userInput = search_input.value;
-    result.innerHTML = ""; 
-    
+    result.innerHTML = "";
+
     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${userInput}`)
         .then((response) => response.json())
         .then((data) => {
             // console.log(data)
-            result.innerHTML+=`<h3>Word:- ${userInput}</h3>`
-            if (Array.isArray(data)) {
-                // console.log(isArray)
-                data.map(word=> {
-                    const meanings =word.meanings;
-                    // console.log(meanings)
-                    if (meanings) {
-                        meanings.forEach(meaning => {
-                            const partOfSpeech = meaning.partOfSpeech;
-                            result.innerHTML += `<p>Part of Speech: ${partOfSpeech}</p>`;
-                            meaning.definitions.forEach(definition => {
-                                result.innerHTML += `<p>-Definition: ${definition.definition}</p>`;
-                            });
+            result.innerHTML += `<h1>Word:- ${userInput}</h1>`
+            data.map(word => {
+                const meanings = word.meanings;
+                // console.log(meanings)
+                if (meanings) {
+                    meanings.forEach(meaning => {
+                        const partOfSpeech = meaning.partOfSpeech;
+                        result.innerHTML += `<h4>Part of Speech: ${partOfSpeech}</h4>`;
+                        meaning.definitions.forEach(definition => {
+                            result.innerHTML += `<p>-Definition: ${definition.definition}</p>`;
                         });
-                    } else {
-                        result.innerHTML += `<p>No meanings found for the word "${userInput}"</p>`;
-                    }
-                });
-            } else {
-                result.innerHTML += `<p>No data found for the word "${userInput}"</p>`;
-            }
+                    });
+                } else {
+                    result.innerHTML += `<p>No meanings found for the word "${userInput}"</p>`;
+                }
+            });
         })
         .catch((error) => {
             result.innerHTML = `<p>Error fetching data: ${error}</p>`;
         });
 });
+
+
 
 
