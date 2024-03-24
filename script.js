518ecbe706209392132c6b5cd5d976b5bc7aba01
devika kumari <devika21@navgurukul.org>
2024-03-24 21:59:59 +0530

    i have done dictionary project

--- script.js
+++ script.js
@@ -1,39 +1,36 @@
 
-
-
 const search_input = document.getElementById("search-input");
 const search_btn = document.getElementById("search-btn");
 const result = document.getElementById("result");
 
 search_btn.addEventListener("click", () => {
     let userInput = search_input.value;
-    result.innerHTML = "";
 
     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${userInput}`)
         .then((response) => response.json())
         .then((data) => {
             // console.log(data)
-            result.innerHTML += `<h1>Word:- ${userInput}</h1>`
-            data.map(word => {
+            result.innerHTML= `<h1>Word:- ${userInput}</h1>`
+
+            data.forEach(function(word){
                 const meanings = word.meanings;
-                // console.log(meanings)
-                if (meanings) {
-                    meanings.forEach(meaning => {
-                        const partOfSpeech = meaning.partOfSpeech;
-                        result.innerHTML += `<h4>Part of Speech: ${partOfSpeech}</h4>`;
-                        meaning.definitions.forEach(definition => {
-                            result.innerHTML += `<p>-Definition: ${definition.definition}</p>`;
-                        });
-                    });
-                } else {
-                    result.innerHTML += `<p>No meanings found for the word "${userInput}"</p>`;
-                }
+                // console.log(meanings)                
+                for (const meaning of meanings) {
+                    const partOfSpeech = meaning.partOfSpeech;
+                    const definitions = meaning.definitions;
+
+                    result.innerHTML += `<h4>${partOfSpeech}</h4>`;
+                    for (const definition of definitions) {
+                        result.innerHTML += `<p>${definition.definition}</p>`;
+                    }
+            }
             });
         })
         .catch((error) => {
             result.innerHTML = `<p>Error fetching data: ${error}</p>`;
         });
 });
+
 
 
 
